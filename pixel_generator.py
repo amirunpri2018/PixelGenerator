7a7feb2673495633bea1f37fedb18d3cce1e0281
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-31 04:09:43 +0900

    update

--- pixel_generator.py
+++ pixel_generator.py
@@ -1,54 +1,10 @@
 import tensorflow as tf
 import numpy as np
-import cv2
+import skimage
+import sys
 
 
 def generator(inputs, stddev):
-
-    def grow(inputs, depth, max_depth):
-
-        inputs = tf.layers.dense(
-            inputs=inputs,
-            units=32,
-            use_bias=False,
-            kernel_initializer=tf.random_normal_initializer(stddev=stddev)
-        )
-
-        inputs = tf.layers.batch_normalization(
-            inputs=inputs,
-            axis=-1,
-            training=True,
-            fused=True
-        )
-
-        inputs = tf.nn.sigmoid(inputs)
-
-        return inputs if depth == max_depth else [grow(inputs, depth + 1, max_depth) for _ in range(2)]
-
-    def shrink(inputs_seq, depth, min_depth):
-
-        inputs = tf.concat(inputs_seq, axis=1) if depth == min_depth else tf.concat(
-            [shrink(inputs, depth - 1, min_depth) for inputs in inputs_seq], axis=1)
-
-        inputs = tf.layers.dense(
-            inputs=inputs,
-            units=32,
-            use_bias=False,
-            kernel_initializer=tf.random_normal_initializer(stddev=stddev)
-        )
-
-        inputs = tf.layers.batch_normalization(
-            inputs=inputs,
-            axis=-1,
-            training=True,
-            fused=True
-        )
-
-        inputs = tf.nn.sigmoid(inputs)
-
-        return inputs
-
-    # inputs = shrink(grow(inputs, 0, 3), 3, 0)
 
     for _ in range(128):
 
@@ -56,14 +12,13 @@ def generator(inputs, stddev):
             inputs=inputs,
             units=32,
             use_bias=False,
-            kernel_initializer=tf.random_normal_initializer(stddev=stddev)
+            kernel_initializer=tf.initializers.random_normal(stddev=stddev)
         )
 
         inputs = tf.layers.batch_normalization(
             inputs=inputs,
             axis=-1,
-            training=True,
-            fused=True
+            training=True
         )
 
         inputs = tf.nn.sigmoid(inputs)
@@ -72,14 +27,13 @@ def generator(inputs, stddev):
         inputs=inputs,
         units=1,
         use_bias=False,
-        kernel_initializer=tf.random_normal_initializer(stddev=stddev)
+        kernel_initializer=tf.initializers.random_normal(stddev=stddev)
     )
 
     inputs = tf.layers.batch_normalization(
         inputs=inputs,
         axis=-1,
-        training=True,
-        fused=True
+        training=True
     )
 
     inputs = tf.nn.sigmoid(inputs)
@@ -87,27 +41,33 @@ def generator(inputs, stddev):
     return inputs
 
 
-inputs = tf.placeholder(tf.float32, [None, 3])
-outputs = generator(inputs, stddev=0.1)
+def generate(image_size):
 
-with tf.Session() as session:
+    inputs = tf.placeholder(tf.float32, [None, 3])
+    outputs = generator(inputs, stddev=0.05)
 
-    session.run(tf.global_variables_initializer())
+    with tf.Session() as session:
 
-    z = np.random.uniform(low=0.0, high=1.0)
+        session.run(tf.global_variables_initializer())
 
-    feed_dict = {
-        inputs: [
-            [x, y, z]
-            for y in np.linspace(0.0, 1.0, 1024)
-            for x in np.linspace(0.0, 1.0, 1024)
-        ]
-    }
+        z = np.random.normal()
 
-    image = session.run(outputs, feed_dict=feed_dict)
-    image = np.reshape(image, [1024, 1024, 1])
-    image = image ** 1.5
+        feed_dict = {
+            inputs: [
+                [x, y, z]
+                for y in np.linspace(0.0, 1.0, image_size)
+                for x in np.linspace(0.0, 1.0, image_size)
+            ]
+        }
 
-    cv2.imwrite("image.png", image * 255.0)
-    cv2.imshow("image", image)
-    cv2.waitKey()
+        image = session.run(outputs, feed_dict=feed_dict)
+        image = np.reshape(image, [image_size, image_size])
+        image = image ** 2
+
+    return image
+
+
+if __name__ == "__main__":
+
+    image = generate(int(sys.argv[1]))
+    skimage.io.imsave("image.png", image)
